57094ac362da94d4de752a700f3bb1b1e843f1a3
Tim Clark <timeeeee@gmail.com>
2015-08-14 15:29:14 -0500

    print solve.py to status.txt instead of standard out

--- solve.py
+++ solve.py
@@ -4,8 +4,9 @@ SOLUTIONS_OUT = "solutions.txt"
 ELEVEN_CHAR_WORDS = load_wordlist("eleven_char_words.txt")
 EIGHT_CHAR_WORDS = load_wordlist("eight_char_words.txt")
 SHORT_WORDS = load_wordlist("short_words.txt")
+STATUS_OUT = open("status.txt", 'w')
 
-def solve(counts, starting_word_index=0):
+def solve(counts, starting_word_index=0, recursions=0):
     """Return a generator that yields all possible solution word sets
 
     It will only check after a certain point in the dictionary, so that we only
@@ -17,8 +18,9 @@ def solve(counts, starting_word_index=0):
         word = SHORT_WORDS[index]
 
         if matches_counts(word, counts):
-            if starting_word_index == 0:
-                print "solutions starting with word {}".format(word)
+            if recursions < 10:
+                STATUS_OUT.write(" " * recursions + word + "\n")
+                STATUS_OUT.flush()
             new_counts = subtract_word(counts, word)
             
             # check to see if this is a solution!
@@ -26,7 +28,7 @@ def solve(counts, starting_word_index=0):
                 yield [word]
             else:
                 # recursively solve for the rest
-                for solution in solve(new_counts, index):
+                for solution in solve(new_counts, index, recursions + 1):
                     yield [word] + solution
 
 
@@ -40,26 +42,28 @@ def solve_qwantzle():
     for eleven_char_word in ELEVEN_CHAR_WORDS:
         # Because the words have been filtered we know it fits
         counts_after_eleven = subtract_word(global_counts, eleven_char_word)
-        print "chose 11 letter word {}".format(eleven_char_word)
+        STATUS_OUT.write("chose 11 letter word {}\n".format(eleven_char_word))
 
         # choose eight letter word
         for eight_char_word in EIGHT_CHAR_WORDS:
-            print "    chose 8 letter word {}".format(eight_char_word)
+            STATUS_OUT.write("    chose 8 letter word {}\n".format(eight_char_word))
+            STATUS_OUT.flush()
             if matches_counts(eight_char_word, counts_after_eleven):
                 counts = subtract_word(counts_after_eleven, eight_char_word)
                 for partial_solution in solve(counts):
                     sentence = " ".join(base_solution + [eleven_char_word] +
                                         [eight_char_word] + partial_solution)
-                    print sentence
                     yield sentence
             else:
-                 print "NNNOOOOOOOPPPPEEEEE"   
+                 STATUS_OUT.write("NNNOOOOOOOPPPPEEEEE\n")
+                 STATUS_OUT.flush()
 
 
 def main():
     with open(SOLUTIONS_OUT, 'w') as outfile:
         for solution in solve_qwantzle():
             outfile.write(solution)
+            outfile.flush()
 
 
 if __name__ == "__main__":
