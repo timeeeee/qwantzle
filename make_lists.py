dd55c2a675c856851f84b762ecd89d665531a6db
Tim Clark <timeeeee@gmail.com>
2016-10-24 23:38:35 -0400

    Only generate word lists for 8-char words and shorter words, subtracting 'fundamental' and 3 I's from the character counts first. Add 'wordlists' make target, and include wordlists in make clean.

--- make_lists.py
+++ make_lists.py
@@ -1,53 +1,64 @@
+"""
+Generate word lists to use for the Qwantzle.
+
+In the clues, Ryan North indicates that all words in the solution are in
+Jadrian's qwantz corpus, and that they're dictionary words. This script will
+output two lists, one with words that are 8 letters long, one with words
+shorter than 8 letters.
+
+Since the first word of the solution is I, and the 11 letter word is
+"fundamental", we can filter out any words that aren't possible within the
+problem letter counts, minus "I", minus "fundamental".
+
+Since the solution contains no apostrophe's, any contractions etc. will be
+filtered out.
+
+In terms of algorithmic complexity, it's probably better to re-filter the short
+words list for each 8-letter word.
+"""
+
 from string import lowercase, digits
 
 from utils import *
 
+# In: All words used by T-Rex
 CORPUS_FILE = "qwantzcorpus"
-FILTERED_OUT = "filtered_words.txt"
-SHORT_OUT = "short_words.txt"
-EIGHT_OUT = "eight_char_words.txt"
-ELEVEN_OUT = "eleven_char_words.txt"
+# In: Blacklist (words to skip)
 BLACKLIST_FILE = "blacklist.txt"
-
-def is_alphabetic(word):
-    for char in set(word):
-        if char not in (lowercase + "I"):
-            return False
-    return True
+# Out: Words that are shorter than 8 characters
+SHORT_OUT = "short_words.txt"
+# Out: Eight character words
+EIGHT_OUT = "eight_char_words.txt"
 
 
-def get_corp(filename):
+def get_corpus(filename):
     with open(filename) as corpus:
         words = ["".join(line.split()[1:]) for line in corpus]
     return filter(is_alphabetic, words)
 
 
-def main():
-    corp_words = get_corp(CORPUS_FILE)
-    letter_counts = make_count_dict(QWANTZLE_COUNTS)
+if __name__ == "__main__":
+    corp_words = [word for word in load_corpus(CORPUS_FILE) if word.isalpha()]
 
-    blacklist = load_wordlist(BLACKLIST_FILE)
+    # Get qwantz counts, minus 3*I and fundamental
+    letter_counts = make_count_dict(QWANTZLE_COUNTS)
+    letter_counts = subtract_word(letter_counts, "I")
+    letter_counts = subtract_word(letter_counts, "I")
+    letter_counts = subtract_word(letter_counts, "I")
+    letter_counts = subtract_word(letter_counts, "fundamental")
+
+    blacklist = set(load_wordlist(BLACKLIST_FILE))
     
     short = []
-    words = []
     eight_letter = []
-    eleven_letter = []
+
     for word in corp_words:
         if matches_counts(word, letter_counts) and word not in blacklist:
             length = len(word)
             if length < 8:
                 short.append(word)
-                words.append(word)
             elif length == 8:
-                words.append(word)
                 eight_letter.append(word)
-            elif length == 11:
-                words.append(word)
-                eleven_letter.append(word)
-
-    with open(FILTERED_OUT, "w") as filtered_out:
-        for word in words:
-            filtered_out.write(word + "\n")
 
     with open(SHORT_OUT, "w") as short_out:
         for word in short:
@@ -57,17 +68,5 @@ def main():
         for word in eight_letter:
             eight_out.write(word + "\n")
 
-    with open(ELEVEN_OUT, "w") as eleven_out:
-        for word in eleven_letter:
-            eleven_out.write(word + "\n")
-            
     print "<8 letter words: {}".format(len(short))
     print "8 letter words:  {}".format(len(eight_letter))
-    print "11 letter words: {}".format(len(eleven_letter))
-    print "total:           {}".format(len(words))
-
-    
-
-
-if __name__ == "__main__":
-    main()
--- makefile
+++ makefile
@@ -1,2 +1,14 @@
-main:
-	g++ solve.cpp -o solve
+all: trie solve
+	g++ -o solve solve.o trie.o
+
+trie: trie.h trie.cpp
+	g++ -c trie.cpp
+
+solve: solve.cpp
+	g++ -c solve.cpp
+
+wordlists: make_lists.py qwantzcorpus
+	python make_lists.py
+
+clean:
+	rm -f solve trie.o solve.o eight_char_words.txt short_words.txt
